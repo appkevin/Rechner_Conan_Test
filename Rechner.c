45ece519290059aa6adad22c1fe9eed2280dea77
appkevin <61727893+appkevin@users.noreply.github.com>
2020-03-17 08:52:13 +0100

    Add files via upload

---
+++ Rechner.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Rechnung.h"
+
+
+int main (){
+
+	int z1=5,z2=5,erg=0;
+	erg = Addieren(z1,z2);
+
+
+return 0;
+}
---
+++ Rechnung.h
@@ -0,0 +1,7 @@
+int Addieren(int Zahl1 , int Zahl2){
+	int erg = 0;
+	
+	erg = Zahl1 + Zahl2;
+	
+	return erg;
+}
